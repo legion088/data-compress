f945d42efe697fb05185f1aa435e6202c177e207
legion088 <ilnar_08@mail.ru>
2023-02-03 00:19:56 +0400

    Added tests

---
+++ tests/test_data_compress.py
@@ -0,0 +1,16 @@
+import pytest
+from data_compress.data_compress import *
+
+
+@pytest.mark.parametrize('exception, arg', [(FileNotFoundError, ' '),
+                                            (FileNotFoundError, 20),
+                                            (ValueError, 'tests_files/empty')])
+def test_exceptions(exception, arg):
+    with pytest.raises(exception):
+        get_folder_files(arg)
+
+
+def test_get_files():
+    files = ['tests_files/files\\data.py',
+             'tests_files/files\\2\\data.json']
+    assert get_folder_files('tests_files/files') == files
