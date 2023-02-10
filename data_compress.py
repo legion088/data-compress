36b66a454571ed841d5cecd6eaa6e846a1732dbf
Ilnar Shamionov <ilnar_08@mail.ru>
2023-02-10 11:57:27 +0400

    Removed the path and extension
    Removed the absolute path and changed the zip extension

--- data_compress.py
+++ data_compress.py
@@ -3,10 +3,9 @@ import zipfile as zip
 from datetime import datetime
 
 
-def data_compress(abspath: str, data: list):
-    filename_compress = f'zip_files_{datetime.now().date()}.zip'
-    abspath_file_compress = os.path.join(abspath, filename_compress)
-    with zip.ZipFile(abspath_file_compress, 'w') as file_zip:
+def data_compress(data: list) -> None:
+    filename_compress = f'zip_files_{datetime.now().date()}.7z'
+    with zip.ZipFile(filename_compress, 'w') as file_zip:
         for item in data:
             file_zip.write(item)
 
@@ -25,4 +24,4 @@ def get_folder_files(abspath: str) -> list:
 if __name__ == '__main__':
     abspath = fr'C:\Users\petrov_pp\Desktop\files'
     data = get_folder_files(abspath)
-    data_compress(abspath, data)
+    data_compress(data)
